550f3bdead2158141d9ca2a892f7a8e736a7cdc9
Igor Florian <igor@telnyx.com>
2020-11-05 21:25:35 -0300

    Initial commit

---
+++ lib/kafka_producer.ex
@@ -0,0 +1,18 @@
+defmodule KafkaProducer do
+  @moduledoc """
+  Documentation for KafkaProducer.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KafkaProducer.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
